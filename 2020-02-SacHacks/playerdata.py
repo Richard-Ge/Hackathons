2450bbd9c5640a29614940ca7391be3ff8ba3c0d
Richard-Ge <61361227+Richard-Ge@users.noreply.github.com>
2020-09-07 15:37:29 -0700

    Add files via upload

---
+++ 2020-02-SacHacks/playerdata.py
@@ -0,0 +1,215 @@
+# print "Top"
+players_string = "All Hail Trey	Total	363	103	193	17	18	49	28	39	20	28	5	252	0.609	0.627	235	\n\
+Arooks	Bucks Gaming	390	119	221	7	7	7	62	35	30	31	1	319	0.706	0.712	286	\n\
+Arsonal	Hawks Talon GC	360	80	122	12	17	189	45	32	33	26	11	174	0.664	0.672	379	\n\
+AuthenticAfrican	Grizz Gaming	384	107	149	21	31	188	55	27	39	31	15	236	0.721	0.726	450	\n\
+BalILikeSeem	Kings Guard Gaming	384	81	159	24	31	12	37	22	17	33	1	228	0.642	0.66	177	\n\
+BearDaBeast	T-Wolves Gaming	384	95	167	23	29	25	145	28	17	57	5	250	0.68	0.695	307	\n\
+BigMeek	Bucks Gaming	222	66	112	6	6	26	14	4	11	15	3	169	0.728	0.737	162	\n\
+Bp	Kings Guard Gaming	384	110	214	25	28	17	127	23	23	71	2	308	0.661	0.68	299	\n\
+BreadwinnerLA	76ers GC	360	52	84	8	9	87	29	31	45	17	17	129	0.72	0.733	257	\n\
+Bsmoove	Warriors Gaming Squad	384	113	206	9	13	10	52	20	26	53	0	289	0.68	0.683	227	\n\
+Bulleyy	Celtics Crossover Gaming	363	53	115	4	6	29	13	29	24	26	9	144	0.609	0.612	129	\n\
+CB13	Warriors Gaming Squad	384	64	135	14	15	20	111	21	17	62	0	177	0.604	0.625	191	\n\
+ChaChingSingh	Heat Check Gaming	72	17	28	0	0	4	4	8	11	2	2	42	0.75	0.75	50	\n\
+chaddynick	Bucks Gaming	390	40	73	4	6	17	25	42	23	30	1	110	0.726	0.727	111	\n\
+Chiquita	Warriors Gaming Squad	96	7	19	0	0	16	1	3	4	3	2	20	0.526	0.526	28	\n\
+Dat Boy Shotz	Hawks Talon GC	288	80	154	24	29	36	26	25	26	33	4	199	0.568	0.597	179	\n\
+Daveed 3x	Blazer5 Gaming	342	33	61	3	5	51	13	23	38	14	13	83	0.656	0.657	154	\n\
+Dayfri	Wizards District Gaming	312	80	136	20	26	67	48	57	26	38	13	194	0.64	0.658	248	\n\
+DDouble	Grizz Gaming	312	59	86	10	10	18	8	24	36	1	1	138	0.744	0.763	173	\n\
+Deedz	Jazz Gaming	387	44	69	2	2	29	31	58	36	17	3	103	0.732	0.737	160	\n\
+Demon JT	Wizards District Gaming	384	45	117	18	20	31	80	16	15	49	2	133	0.491	0.529	138	\n\
+Detoxys	Lakers Gaming	318	47	90	10	11	39	10	36	16	10	10	123	0.628	0.648	144	\n\
+DevGoss	Total	384	97	186	9	10	9	64	16	29	48	4	240	0.621	0.63	208	\n\
+Dimez	Mavs Gaming	384	110	186	20	25	12	139	24	22	74	2	267	0.664	0.678	287	\n\
+Doza	Raptors Uprising GC	264	46	99	8	9	19	67	12	11	40	2	121	0.571	0.588	126	\n\
+DT	Magic Gaming	384	93	159	10	11	55	19	33	43	21	13	229	0.689	0.699	271	\n\
+FEAST	T-Wolves Gaming	384	72	106	10	13	139	52	39	41	28	18	167	0.741	0.747	352	\n\
+G O O F Y 7 5 7	Knicks Gaming	336	94	130	13	26	208	70	22	24	58	17	201	0.723	0.711	413	\n\
+Gilly	Wizards District Gaming	360	87	121	22	25	162	37	12	16	21	16	200	0.736	0.758	373	\n\
+Gliz	Jazz Gaming	315	70	122	4	5	59	30	22	23	19	5	169	0.676	0.68	214	\n\
+Godddof2k	Cavs Legion GC	387	48	79	9	9	26	27	53	57	12	4	116	0.677	0.699	187	\n\
+Gradient	Warriors Gaming Squad	384	59	106	6	8	29	16	53	44	20	3	135	0.608	0.616	158	\n\
+GRANT MONSTER	Mavs Gaming	48	5	5	0	0	13	0	1	2	4	2	13	1.3	1.3	26	\n\
+HazzaUK	Knicks Gaming	336	51	94	7	10	24	22	36	32	11	4	134	0.676	0.681	159	\n\
+Hood	Total	360	88	181	13	15	25	64	20	24	62	6	218	0.566	0.581	180	\n\
+Hotshot	Heat Check Gaming	360	97	175	19	27	172	84	39	36	66	13	225	0.589	0.602	378	\n\
+iamadamthe1st	Knicks Gaming	384	70	156	14	17	10	110	24	18	80	0	191	0.567	0.584	160	\n\
+Idrisdagoat6	Knicks Gaming	288	64	100	12	16	53	16	17	23	20	4	151	0.695	0.705	187	\n\
+iiNsaniTTy	Pistons GT	336	71	126	11	14	5	30	32	23	25	2	168	0.623	0.636	145	\n\
+iiTzChas	NetsGC	48	7	14	0	0	2	1	3	1	2	0	18	0.643	0.643	13	\n\
+Jaacko	Pistons GT	288	43	92	10	11	10	23	5	8	11	1	119	0.592	0.614	100	\n\
+Jay a FooL	Hawks Talon GC	336	49	87	2	2	13	10	22	21	15	4	129	0.73	0.734	124	\n\
+Jayrod	Grizz Gaming	384	57	129	5	5	40	29	17	20	20	5	155	0.581	0.591	157	\n\
+Jin	Warriors Gaming Squad	360	66	107	10	11	83	27	14	19	15	6	160	0.701	0.715	238	\n\
+JMoneyRep817	Total	360	111	200	15	18	13	63	28	24	44	1	273	0.645	0.657	238	\n\
+JoJo	T-Wolves Gaming	384	76	112	4	4	85	24	23	27	29	9	177	0.772	0.778	257	\n\
+Jomar12 PR	Blazer5 Gaming	168	32	56	2	3	18	13	18	14	4	5	81	0.705	0.707	102	\n\
+Joseph	Pistons GT	384	75	116	17	18	70	33	31	26	21	12	194	0.763	0.783	272	\n\
+KelMav	Total	312	60	131	8	8	16	28	16	8	12	3	153	0.553	0.569	125	\n\
+Kenny Got Work	Raptors Uprising GC	384	97	188	14	16	18	87	17	27	54	6	253	0.636	0.649	244	\n\
+Kev	Lakers Gaming	390	92	204	13	15	19	22	20	33	27	5	251	0.583	0.596	189	\n\
+KevDontMiss	Jazz Gaming	144	28	52	2	2	6	17	3	6	17	1	71	0.663	0.671	60	\n\
+King Peroxide	Blazer5 Gaming	270	55	113	5	7	11	16	31	14	18	0	143	0.611	0.616	106	\n\
+KingCamRoyalty	Magic Gaming	336	93	119	6	10	142	33	25	21	22	14	194	0.79	0.786	352	\n\
+KingQuai614	Raptors Uprising GC	360	53	95	7	9	110	41	37	47	36	11	122	0.605	0.616	251	\n\
+Kontrul	Lakers Gaming	363	43	105	18	22	20	42	30	17	43	2	115	0.462	0.501	87	\n\
+l 24k Dropoff l	Heat Check Gaming	360	70	127	10	11	45	45	37	27	33	1	175	0.65	0.664	202	\n\
+Lav	NetsGC	363	89	177	20	29	18	63	19	15	47	3	237	0.613	0.624	192	\n\
+LavishPhenom	Blazer5 Gaming	390	53	92	10	10	9	11	46	41	8	4	131	0.658	0.679	149	\n\
+Lord Beezus	Pacers Gaming	384	58	101	4	5	97	32	39	24	15	13	132	0.634	0.64	239	\n\
+Lotty	Total	411	90	130	7	9	17	26	50	37	12	5	214	0.796	0.799	245	\n\
+LYKaPRO	Cavs Legion GC	144	18	44	5	6	4	36	5	8	16	0	49	0.5	0.525	54	\n\
+MaJes7ic	Heat Check Gaming	384	69	128	8	11	56	36	33	28	32	9	165	0.613	0.621	200	\n\
+Mama Im Dat Man	Blazer5 Gaming	390	132	235	42	50	18	185	18	23	77	4	369	0.696	0.718	411	\n\
+Man_in_a_car	Pacers Gaming	144	29	62	1	2	8	12	6	9	10	2	80	0.637	0.636	67	\n\
+Matty	Pacers Gaming	240	50	83	10	16	12	57	4	8	23	0	124	0.687	0.689	139	\n\
+May	Pistons GT	312	71	101	6	11	146	49	40	18	36	8	149	0.708	0.704	299	\n\
+Mel East	Celtics Crossover Gaming	387	62	98	14	15	19	14	45	41	10	6	163	0.76	0.779	196	\n\
+Mo	Mavs Gaming	384	90	185	2	2	20	58	35	20	36	3	236	0.632	0.635	206	\n\
+MOAM	Total	264	68	118	7	9	20	23	18	24	27	2	174	0.708	0.713	164	\n\
+Mootyy	Lakers Gaming	390	66	104	10	13	176	51	47	29	49	24	145	0.649	0.661	335	\n\
+MrStylez	Total	360	59	139	4	7	12	78	39	19	52	5	146	0.511	0.514	125	\n\
+NachoTraynor	T-Wolves Gaming	48	5	10	2	3	6	2	3	1	1	0	13	0.55	0.574	15	\n\
+NateKahl	NetsGC	387	82	120	12	13	31	17	51	41	15	4	190	0.742	0.756	229	\n\
+Newdini33	76ers GC	384	46	74	2	2	19	14	32	33	9	4	112	0.743	0.748	145	\n\
+NO xAUTOGRAPHSx	Celtics Crossover Gaming	387	68	99	8	11	187	45	18	30	30	19	151	0.722	0.727	368	\n\
+oCASSIUS	76ers GC	24	13	24	0	0	10	4	0	2	3	0	36	0.75	0.75	38	\n\
+oFAB	Celtics Crossover Gaming	363	129	224	34	44	18	188	24	38	78	0	334	0.67	0.686	395	\n\
+oLarry	Cavs Legion GC	387	68	109	23	31	103	56	29	35	36	13	165	0.651	0.673	287	\n\
+OneWildWalnut	Blazer5 Gaming	390	124	172	7	12	202	27	26	35	43	7	255	0.721	0.719	430	\n\
+OOC Slim	Raptors Uprising GC	168	31	59	5	5	16	10	11	12	7	1	78	0.619	0.637	82	\n\
+OriginalMalik	Knicks Gaming	384	95	201	9	14	16	37	29	30	61	4	247	0.592	0.596	162	\n\
+PaulB	Wizards District Gaming	96	12	22	2	2	24	10	7	9	7	3	29	0.614	0.634	58	\n\
+PeteBeBallin	Mavs Gaming	384	107	145	15	21	179	37	38	38	38	18	229	0.738	0.742	419	\n\
+Plondo	Bucks Gaming	390	130	221	24	35	145	70	46	36	42	24	293	0.609	0.62	424	\n\
+ProFusion	Celtics Crossover Gaming	387	102	172	9	12	20	16	38	34	17	3	266	0.747	0.75	249	\n\
+Radiant	76ers GC	384	123	230	18	20	9	177	17	21	68	1	320	0.657	0.67	351	\n\
+Ramo	Pacers Gaming	384	106	193	13	17	101	87	46	33	35	9	236	0.578	0.589	340	\n\
+Rando	Hawks Talon GC	360	82	173	18	25	11	61	15	15	56	2	221	0.587	0.601	156	\n\
+ReeseDaGod	Wizards District Gaming	384	134	232	11	16	14	61	17	25	63	4	331	0.69	0.692	269	\n\
+Reizey	Magic Gaming	384	105	194	17	23	25	156	9	9	73	1	277	0.67	0.679	300	\n\
+Ria	Jazz Gaming	387	108	171	25	31	164	56	46	35	35	28	251	0.661	0.68	430	\n\
+Roman	Kings Guard Gaming	48	5	13	0	0	8	0	0	0	0	2	15	0.577	0.577	17	\n\
+RSG	Bucks Gaming	168	12	14	3	4	66	19	11	6	10	2	28	0.893	0.888	108	\n\
+Rux	Mavs Gaming	336	57	97	0	1	55	13	23	20	19	13	143	0.737	0.734	184	\n\
+Safiya4ya	Lakers Gaming	99	19	29	1	1	2	5	7	12	6	2	43	0.724	0.73	48	\n\
+Savage	Cavs Legion GC	243	38	64	3	5	9	42	7	14	29	0	89	0.672	0.672	97	\n\
+Sherm	Mavs Gaming	384	48	70	6	8	17	15	34	31	10	6	116	0.786	0.789	151	\n\
+Shockey	NetsGC	363	64	98	10	12	107	34	29	22	24	8	149	0.709	0.721	260	\n\
+Shuttles	NetsGC	387	52	82	9	15	115	46	38	39	35	16	126	0.713	0.711	271	\n\
+Sick x 973	Total	408	114	183	13	15	120	42	59	37	38	13	271	0.705	0.715	374	\n\
+SlayIsland	Bucks Gaming	390	39	72	14	14	26	44	28	21	42	0	103	0.618	0.659	119	\n\
+splashking	Pistons GT	216	45	85	9	17	34	51	9	20	42	2	106	0.571	0.573	123	\n\
+Steez	76ers GC	384	80	115	17	27	156	33	30	29	33	10	181	0.713	0.713	331	\n\
+Strainer	Cavs Legion GC	387	103	174	10	13	20	66	16	14	44	7	255	0.704	0.709	244	\n\
+Suavy	Celtics Crossover Gaming	48	5	9	0	0	5	1	5	5	3	2	13	0.722	0.722	19	\n\
+Swizurk	Pacers Gaming	384	101	161	12	17	25	46	12	15	37	1	266	0.789	0.789	251	\n\
+ToXsiK	Total	336	40	73	11	11	21	12	50	26	13	2	101	0.616	0.649	116	\n\
+TsJosh	Raptors Uprising GC	360	46	75	3	4	13	13	38	27	6	3	108	0.7	0.703	128	\n\
+TuckerLocksUp	Magic Gaming	72	10	20	1	2	11	0	6	4	7	3	24	0.575	0.575	24	\n\
+TURNUPDEFENSE	T-Wolves Gaming	384	62	101	10	13	20	25	37	37	11	3	152	0.703	0.712	184	\n\
+Type	Warriors Gaming Squad	312	73	118	10	17	144	24	47	34	25	21	160	0.636	0.638	306	\n\
+UCMANNY	Magic Gaming	384	41	78	7	7	14	18	33	24	12	0	107	0.641	0.66	114	\n\
+UniversaIPhenom	Grizz Gaming	168	18	38	3	4	31	4	9	10	8	4	43	0.526	0.541	63	\n\
+USERPICK	Wizards District Gaming	384	52	78	0	0	17	16	59	40	6	1	125	0.801	0.801	167	\n\
+Vandi	Grizz Gaming	384	108	203	22	27	18	134	22	15	74	3	285	0.648	0.663	281	\n\
+Vert	Lakers Gaming	390	67	137	16	22	23	86	29	25	59	6	168	0.555	0.573	173	\n\
+Wavy.zay	NetsGC	387	95	182	22	27	11	93	23	17	68	5	241	0.602	0.622	207	\n\
+WoLF 74	Pacers Gaming	384	64	100	13	13	20	21	40	30	9	3	158	0.725	0.747	187	\n\
+worthingcolt	Kings Guard Gaming	384	88	130	10	14	189	51	33	22	45	5	186	0.677	0.683	362	\n\
+XxSTL2LAxX	Hawks Talon GC	216	37	66	2	4	38	37	15	10	20	7	82	0.606	0.605	123	\n\
+Yeah I Compete	Jazz Gaming	387	80	145	8	9	18	128	27	4	65	3	196	0.648	0.658	218	\n\
+YEYNotGaming	Knicks Gaming	192	15	29	3	4	5	9	21	8	5	3	40	0.638	0.65	45	\n\
+Yusuf_Scarbz	Kings Guard Gaming	336	45	73	0	0	46	14	30	28	14	24	124	0.849	0.849	194	\n\
+Zakyy610	Kings Guard Gaming	384	43	68	1	2	11	13	48	29	5	4	107	0.779	0.777	133	\n\
+ZDS	76ers GC	384	107	199	6	9	10	32	17	22	19	1	280	0.688	0.69	231	"
+
+player_list = players_string.split("\n")
+# print team_list
+# print len(player_list)
+for i in range(0, 126):
+    player_list[i] = player_list[i].split("\t")     # CHANGE THIS TO ", " IF .CSV!
+# print player_list
+query = "Dat Boy Shotz" # CHANGE THIS VALUE TO
+# KINGS GUARD PLAYERS
+"""worthingcolt
+Bp
+Yusuf_Scarbz
+BalILikeSeem
+Zakyy610
+Roman
+"""
+print "Player:", query
+print "All rankings are out of 126 (#1-126)"
+# This part finds the index of the above player
+player_index = -1
+for index in range(0, 126):
+    if player_list[index][0] == query:
+        player_index = index
+# Now player_index is at the player we want to find
+
+# OFFENSE: POINTS PER MINUTE CALCULATION
+print "Offense:"
+player_ppms = []
+for index in range(0, 126):
+    # print player_list[index][13], player_list[index][2]
+    points = float(player_list[index][13])
+    mins = float(player_list[index][2])
+    player_ppms.append( points / mins ) # PTS / MIN
+# print player_ppms
+specific_ppm = player_ppms[player_index]
+player_ppms.sort(reverse = True, key = float)
+specific_ppm_rank = player_ppms.index(specific_ppm)
+print "Ranking for Points per Minute:", specific_ppm_rank
+
+# DEFENSE: STEAL, BLOCK
+print "Defense:"
+player_steal = []
+player_block = []
+for index in range(0, 126):
+    steals = player_list[index][10]
+    blocks = player_list[index][12]
+    player_steal.append(steals)
+    player_block.append(blocks)
+specific_steal = player_list[player_index][10]
+specific_block = player_list[player_index][12]
+# print specific_block, specific_steal
+player_steal.sort(reverse = True, key = int)
+player_block.sort(reverse = True, key = int)
+player_steal_rank = player_steal.index(specific_steal)+1
+player_block_rank = player_block.index(specific_block)+1
+print "Ranking for Steals:", player_steal_rank,", Blocks:", player_block_rank
+
+# SUPPORT: REBOUNDS, ASSISTS
+print "Support:"
+player_rebound = []
+player_assist = []
+for index in range(0, 126):
+    rebounds = player_list[index][7]
+    assists = player_list[index][8]
+    player_rebound.append(rebounds)
+    player_assist.append(assists)
+specific_rebound = player_list[player_index][7]
+specific_assist = player_list[player_index][8]
+player_rebound.sort(reverse = True, key = int)
+player_assist.sort(reverse = True, key = int)
+player_rebound_rank = player_rebound.index(specific_rebound)+1
+player_assist_rank = player_assist.index(specific_assist)+1
+print "Ranking for Rebound:", player_rebound_rank,", Assists:", player_assist_rank
+
+# OVERALL SCORE (EFFICIENCY)
+print "Rank for Overall Efficiency:"
+player_eff = []
+for index in range(0, 126):
+    eff = player_list[index][16]
+    player_eff.append(eff)
+specific_eff = player_list[player_index][16]
+player_eff.sort(reverse = True, key = int)
+# print player_eff
+player_eff_rank = player_eff.index(specific_eff)+1
+print player_eff_rank
+
+
+
